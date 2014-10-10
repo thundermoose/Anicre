d6c92867fb91a62cac2ef5446e4feffcb4bc7ffe
Daniel Sääf <daniel.saaf@chalmers.se>
2014-10-10 11:43:14 +0200

    Added function to read and find state for nn

--- anicr/src/dumpnlj.c
+++ anicr/src/dumpnlj.c
@@ -20,8 +20,59 @@ typedef struct nlj_hash_item_t
   double   _value;
 } nlj_hash_item;
 
-nlj_hash_item *_nlj_items = NULL;
-size_t     _num_nlj_items = 0;
+nlj_hash_item *_nlj_items_nn = NULL;
+size_t     _num_nlj_items_nn = 0;
+nlj_hash_item *_nlj_items_pp = NULL;
+size_t     _num_nlj_items_pp = 0;
+nlj_hash_item *_nlj_items_np = NULL;
+size_t     _num_nlj_items_np = 0;
+
+
+double findState(nlj_hash_item *nlj_items, size_t num_nlj_items,int i1,int i2, int j1,int j2, int J1, int J2,int jtrans)
+{
+  uint64_t savedkey=0;
+    for (size_t i = 0; i < num_nlj_items; i++)
+  //    	       for (size_t i = 0; i <1; i++)
+    {
+      //   printf("i %d %d\n",(int)i,(int)num_nlj_items);
+   
+      uint64_t key = nlj_items[i]._key;
+      
+      int nlj_a1, nlj_a2, nlj_c1, nlj_c2;
+      int anni_j, crea_j;
+      int key_jtrans;
+      
+      nlj_a1 = (key >>  0) & 0x7ff;
+      nlj_a2 = (key >> 11) & 0x7ff;
+      nlj_c1 = (key >> 22) & 0x7ff;
+      nlj_c2 = (key >> 33) & 0x7ff;
+      anni_j = (key >> 44) &  0x7f;
+      crea_j = (key >> 51) &  0x7f;
+      key_jtrans = (int) (key >> 58);
+      //For nn
+      //   printf("TESTING %d %d %d %d %d \n", (int)i,nlj_a1,nlj_a2,nlj_c1,nlj_c2);
+      if (key_jtrans == jtrans && nlj_c1==i1 && nlj_c2==i2 && nlj_a1==j1 && nlj_a2==j2 && anni_j==J1 && crea_j==J2)
+	{
+	  printf("Found state %d %d and %d %d ",i1,i2,j1,j2);
+	  if(savedkey!=0){
+	    printf("ERROR: More then one key found!");
+	    return 0;
+	   }
+	  savedkey=i;
+	}
+      //printf("test2\n");
+    }
+    //  printf("Saved key %d /n",(int)savedkey);
+  if(savedkey==0)
+    {
+    return 0;
+    }
+  else
+    {
+    printf("Klart!");
+    return (double)nlj_items[savedkey]._value;
+    }
+  }
 
 int compare_nlj_item(const void *p1, const void *p2)
 {
@@ -83,27 +134,28 @@ int compare_nlj_item(const void *p1, const void *p2)
   return 0;
 }
 
-int main()
-{
 
-
-  int fd = open ("nlj_out.bin",O_RDONLY);
-  
+void*  readDumpfile(char *filename, size_t *num_nlj_items)
+{ 
+  nlj_hash_item *nlj_items=0;
+  printf("Address1: %p\n",(void*)nlj_items);
+  int fd = open (filename,O_RDONLY);
   if (fd == -1)
     {
       perror("open");
       exit(1);
     }
 
-  full_read (fd, &_num_nlj_items, sizeof (_num_nlj_items));
+  full_read (fd, num_nlj_items, sizeof (*num_nlj_items));
 
-  printf ("%zd nlj items.\n", _num_nlj_items);
+  printf ("%zd nlj items.\n", *num_nlj_items);
 
-  size_t sz_nlj_items = sizeof (nlj_hash_item) * _num_nlj_items;
+  size_t sz_nlj_items = sizeof (nlj_hash_item) * *num_nlj_items;
+  printf("test %d",(int)sz_nlj_items);
+  nlj_items = (nlj_hash_item *) malloc (sz_nlj_items);
+  printf("Address1: %p\n",(void*)nlj_items);
 
-  _nlj_items = (nlj_hash_item *) malloc (sz_nlj_items);
-
-  if (!_nlj_items)
+  if (!nlj_items)
     {
       fprintf (stderr,
 	       "Memory allocation error (%zd bytes).\n", sz_nlj_items);
@@ -111,19 +163,32 @@ int main()
     }
  
 
-  full_read (fd, _nlj_items, sizeof (nlj_hash_item) * _num_nlj_items);
+  full_read (fd, nlj_items, sizeof (nlj_hash_item) * *num_nlj_items);
 
   close (fd);
+ 
+ qsort (nlj_items, *num_nlj_items, sizeof (nlj_hash_item), compare_nlj_item);
+for (int i=0;i<(int)*num_nlj_items;i++){
 
-  qsort (_nlj_items, _num_nlj_items, sizeof (nlj_hash_item), compare_nlj_item);
+   printf("test %f",nlj_items[i]._value);
+  }
+  printf("Address1: %p\n",(void*)nlj_items);
 
+  return nlj_items;
+}
+
+int main()
+{
+
+  char filename[14]="nlj_out-nn.bin";
+  _nlj_items_nn=readDumpfile(filename,&_num_nlj_items_nn);
+  printf("test %d",(int) _num_nlj_items_nn);
   int jtrans_min = abs(CFG_2J_INITIAL - CFG_2J_FINAL);
   int jtrans_max = CFG_2J_INITIAL + CFG_2J_FINAL;
 
   int jtrans;
 
   int mtrans = CFG_2M_INITIAL - CFG_2M_FINAL;
-
   if (abs(mtrans) > jtrans_max)
     {
       fprintf (stderr, "FIXME: abs(mtrans) > jtrans_max.\n");
@@ -142,29 +207,26 @@ int main()
 	  gsl_sf_coupling_3j_e(CFG_2J_INITIAL,  jtrans,  CFG_2J_FINAL,
 			       CFG_2M_INITIAL, -mtrans, -CFG_2M_FINAL,
 			       &result);
-	
 	if (ret != GSL_SUCCESS)
 	  {
 	    fprintf (stderr,"ERR! %d\n", ret);
 	    exit(1);
 	  }
-
 	int sign = 1 - ((CFG_2J_INITIAL/* - jtrans*/ + CFG_2M_FINAL) & 2);
-
+       
 	mult = 1 / (result.val) * sign;
       }
-
-      size_t i;
-      int T1;
-      int J1;
+      
+      int Tab;
+      int Jab;
       int pi1;
     
       int twob1=0;
       int twob2=0;
       printf("num %d",CFG_NUM_NLJ_STATES);
 	      for(pi1=1;pi1>=-1;pi1=pi1-2){
-	        for(J1=0; J1<=CFG_MAX_J;J1++){
-		  for (T1=0;T1<=1;T1++){
+	        for(Jab=0; Jab<=CFG_MAX_J;Jab++){
+		  for (Tab=0;Tab<=1;Tab++){
 		    for (int i1 = 0; i1<CFG_NUM_NLJ_STATES; i1++)
 		      {
 			for(int i2 = i1; i2<CFG_NUM_NLJ_STATES; i2++)
@@ -175,18 +237,18 @@ int main()
 		    int ni2=_table_nlj_states[i2]._n;
 		    int ji1=_table_nlj_states[i1]._j;
 		    int ji2=_table_nlj_states[i2]._j;
-		    if(ji1+ji2<2*J1){continue;}
-		    if(abs(ji1-ji2)>2*J1){continue;}
+		    if(ji1+ji2<2*Jab){continue;}
+		    if(abs(ji1-ji2)>2*Jab){continue;}
 		    if(pow(-1,(li1+li2))!=pi1){continue;}
-		    if(i1==i2 && pow(-1,(J1+T1))!=-1){continue;}
+		    if(i1==i2 && pow(-1,(Jab+Tab))!=-1){continue;}
 		    if(2*ni1+li1+2*ni2+li2>CFG_MAX_SUM_E){continue;}
 		    //		    printf("a,b: %d: %d %d %d    %d: %d %d %d\n",i1+1,ni1,li1,ji1,i2+1,ni2,li2,ji2);
 		    //		    printf("1:Two-body state %d: %d %d J=%d T=%d pi=%d \n",twob1,i1+1,i2+1,J1,T1,pi1);
 		    twob1++;
 		    twob2=0;
 		    for(int pi2=1;pi2>=-1;pi2=pi2-2){
-		      for(int J2=0; J2<=CFG_MAX_J;J2++){
-			for (int T2=0;T2<=1;T2++){
+		      for(int Jcd=0; Jcd<=CFG_MAX_J;Jcd++){
+			for (int Tcd=0;Tcd<=1;Tcd++){
 			  for (int j1 = 0; j1<CFG_NUM_NLJ_STATES; j1++)
 			    {
 			for(int j2 = j1; j2<CFG_NUM_NLJ_STATES; j2++)
@@ -198,57 +260,27 @@ int main()
 		    int nj2=_table_nlj_states[j2]._n;
 		    int jj1=_table_nlj_states[j1]._j;
 		    int jj2=_table_nlj_states[j2]._j;
-		    if(jj1+jj2<2*J2){continue;}
-		    if(abs(jj1-jj2)>2*J2){continue;}
+		    if(jj1+jj2<2*Jcd){continue;}
+		    if(abs(jj1-jj2)>2*Jcd){continue;}
 		    if(pow(-1,(lj1+lj2))!=pi2){continue;}
-		    if(j1==j2 && pow(-1,(J2+T2))!=-1){continue;}
+		    if(j1==j2 && pow(-1,(Jcd+Tcd))!=-1){continue;}
 		    if(2*nj1+lj1+2*nj2+lj2>CFG_MAX_SUM_E){continue;}
-		    //		    printf("c,d: %d: %d %d %d    %d: %d %d %d\n",j1+1,nj1,lj1,jj1,j2+1,nj2,lj2,jj2);
-		    //	    printf("2:Two-body state %d:%d %d J=%d T=%d pi=%d \n",twob2,j1+1,j2+1,J2,T2,pi2);
 		    twob2++;
+		    double value=findState(_nlj_items_nn, _num_nlj_items_nn, i1, i2,  j1, j2, Jab,Jcd,jtrans);
 
-		    //Energy 
-	      //Parity
-	      //spin
-		    
-
-	        for (i = 0; i < _num_nlj_items; i++)
-		    //	       for (i = 0; i <1; i++)
-		{
-		  uint64_t key = _nlj_items[i]._key;
-		  
-		  int nlj_a1, nlj_a2, nlj_c1, nlj_c2;
-		  int anni_j, crea_j;
-		  int key_jtrans;
-	  
-		  nlj_a1 = (key >>  0) & 0x7ff;
-		  nlj_a2 = (key >> 11) & 0x7ff;
-		  nlj_c1 = (key >> 22) & 0x7ff;
-		  nlj_c2 = (key >> 33) & 0x7ff;
-		  anni_j = (key >> 44) &  0x7f;
-		  crea_j = (key >> 51) &  0x7f;
-		  key_jtrans = (int) (key >> 58);
 		  //For nn
-		  if (key_jtrans == jtrans && nlj_c1==i1 && nlj_c2==i2 && nlj_a1==j1 && nlj_a2==j2 && T2==1 &&T1==1 )
-		    {	  
-		      double value = _nlj_items[i]._value;
+		  
 		      if (value){
-			//		      if(crea_j==J1 &&anni_j==J2){
-			//Testing for NN-case
-			//int m3=2;
+	
+			printf("test %d %d %d \n",twob1,twob2,Tab);
 		       
-			//	int ret =
-			//  gsl_sf_coupling_3j_e(CFG_2J_INITIAL,  jtrans,  CFG_2J_FINAL,
-			//      CFG_2M_INITIAL, -mtrans, -CFG_2M_FINAL,
-			//     &result);
-			printf("test %d %d %d \n",twob1,twob2,T1);
 
-		        printf ("Create %3d %3d : %2d | Annihilate %3d %3d : %2d = %11.6f\n",
-			      nlj_c1+1, nlj_c2+1, crea_j,
-			      nlj_a1+1, nlj_a2+1, anni_j,
-			      mult * value);
+			printf ("Create %3d %3d : %2d | Annihilate %3d %3d : %2d = %11.6f\n",
+			    i1+1, i2+1, Jab,
+			    j1+1, j2+1, Jcd,
+			    mult * value);
 				}
-		    }}
+		    
 	  	  }
 	    }
 	      }
@@ -261,7 +293,7 @@ int main()
 	}
     }
 
-  free (_nlj_items);
+  free (_nlj_items_nn);
 
   return 0;
 }
