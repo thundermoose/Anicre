2cfac879be52b086f03b3c383eb2ed6148514a5a
Daniel Sääf <daniel.saaf@chalmers.se>
2015-09-27 22:19:21 +0200

    Added dumpnlj.h

---
+++ anicr/src/dumpnlj.h
@@ -0,0 +1,13 @@
+typedef struct nlj_hash_item_t
+{
+  uint64_t _key;
+  double   _value;
+} nlj_hash_item;
+
+int compare_nlj_item(const void *p1, const void *p2);
+
+double findState2(nlj_hash_item *nlj_items, size_t num_nlj_items,int i1,int i2, int j1,int j2, int J1, int J2,int jtrans);
+
+double norm(int na,int la,int ja,int nb,int lb,int jb,int J,int T);
+
+void*  readDumpfile(char *filename, size_t *num_nlj_items);
