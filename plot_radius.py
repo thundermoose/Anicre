4dab39dec09dd42c3203c41bfdd1a5d6ab8ed019
Daniel Sääf <daniel.saaf@chalmers.se>
2015-10-12 16:46:24 +0200

    Corrected label in plot_radius.py

--- plot_radius.py
+++ plot_radius.py
@@ -5,7 +5,8 @@ A=np.loadtxt("output_r_0.txt")
 
 fig=plt.figure()
 
-plt.plot(A[:,0],A[:,1],label='Neutron')
+plt.plot(A[:,0],A[:,1],label='Protron')
 plt.plot(A[:,0],A[:,2],label='Neutron')
+
 plt.legend()
 plt.savefig("radius.pdf")
