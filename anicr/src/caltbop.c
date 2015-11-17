9b3cc7bdabe717fb0431656168000cca7379e011
Daniel Sääf <daniel.saaf@chalmers.se>
2015-11-17 11:53:13 +0100

    Read m.e. from TBME.int

--- anicr/src/caltbop.c
+++ anicr/src/caltbop.c
@@ -273,6 +273,143 @@ int twob()
   return 0;
 }
 
+int computeTBME(){
+  for (jtrans = jtrans_min; jtrans <= jtrans_max; jtrans += 2)
+    {
+      showJtrans=1;
+      
+      double mult;
+      {
+	gsl_sf_result result;
+	
+	int ret =
+	  gsl_sf_coupling_3j_e(CFG_2J_INITIAL,  jtrans,  CFG_2J_FINAL,
+			       CFG_2M_INITIAL,  mtrans, -CFG_2M_FINAL,
+			       &result);
+	if (ret != GSL_SUCCESS)
+	  {
+	    fprintf (stderr,"ERR! %d\n", ret);
+	     exit(1); 
+	  }
+	int sign = 1 - ((CFG_2J_INITIAL - jtrans + CFG_2M_FINAL) & 2);
+       
+	mult = 1.0 / (result.val) * sign; 
+	if(result.val==0.0){continue;}
+      }
+      twob1=0;
+      int twob2=0;
+   
+      for(pi1=1;pi1>=-1;pi1=pi1-2){
+
+	int li1=_table_nlj_states[i1]._l;
+      	int li2=_table_nlj_states[i2]._l;
+	int ni1=_table_nlj_states[i1]._n;
+	int ni2=_table_nlj_states[i2]._n;
+	int ji1=_table_nlj_states[i1]._j;
+       	int ji2=_table_nlj_states[i2]._j;
+	if(ji1+ji2<2*Jab){continue;}
+	if(abs(ji1-ji2)>2*Jab){continue;}
+	if(pow(-1,(li1+li2))!=pi1){continue;}
+	if(i1==i2 && pow(-1,(Jab+Tab))!=-1){continue;}
+	if(2*ni1+li1+2*ni2+li2>CFG_MAX_SUM_E){continue;}
+	
+	twob1++;
+	twob2=0;
+	for(int pi2=1;pi2>=-1;pi2=pi2-2){
+      	  for(int Jcd=0; Jcd<=CFG_MAX_J;Jcd++){
+	    int lj1=_table_nlj_states[j1]._l;
+	    int lj2=_table_nlj_states[j2]._l;
+	    int nj1=_table_nlj_states[j1]._n;
+	    int nj2=_table_nlj_states[j2]._n;
+	    int jj1=_table_nlj_states[j1]._j;
+	    int jj2=_table_nlj_states[j2]._j;
+	    if(jj1+jj2<2*Jcd){continue;}
+	    if(abs(jj1-jj2)>2*Jcd){continue;}
+	    if(pow(-1,(lj1+lj2))!=pi2){continue;}
+	    if(j1==j2 && pow(-1,(Jcd+Tcd))!=-1){continue;}
+	    if(2*nj1+lj1+2*nj2+lj2>CFG_MAX_SUM_E){continue;}
+			  
+	    twob2++;
+	    double value_nn=findState2(_nlj_items_nn, _num_nlj_items_nn, i1, i2,  j1, j2, 2*Jab,2*Jcd,jtrans);
+	    double value_pp=findState2(_nlj_items_pp, _num_nlj_items_pp, i1, i2,  j1, j2, 2*Jab,2*Jcd,jtrans);
+	    double value_np=findState2(_nlj_items_np, _num_nlj_items_np, i1, i2,  j1, j2, 2*Jab,2*Jcd,jtrans);  
+	    double rev1_np=0.0;
+	    double rev2_np=0.0;
+	    double rev3_np=0.0;
+	    
+	    if(i1!=i2){
+	      rev1_np=findState2(_nlj_items_np,_num_nlj_items_np,i2,i1,j1,j2,2*Jab,2*Jcd,jtrans);  
+	      if(rev1_np!=0.0){rev1_np=rev1_np*pow(-1.,-(ji1+ji2)/2+Jab+Tab-1);}
+	    }
+			  
+	    if(j2!=j1){  
+	      rev2_np=findState2(_nlj_items_np,_num_nlj_items_np,i1,i2,j2,j1,2*Jab,2*Jcd,jtrans);
+	      if(rev2_np!=0.0){rev2_np=rev2_np*pow(-1.,-(jj1+jj2)/2+Jcd+Tcd-1);}
+	    }
+					  
+	    if(i1!=i2&&j2!=j1){
+	      rev3_np=findState2(_nlj_items_np,_num_nlj_items_np,i2,i1,j2,j1,2*Jab,2*Jcd,jtrans);
+	      if(rev3_np!=0.0){rev3_np=rev3_np*pow(-1.,-(ji1+ji2+jj1+jj2)/2+Jab+Jcd+Tab+Tcd); }  
+	    }
+		   
+	    double clebsch_nn=0.0;
+	    double clebsch_pp=0.0;
+	    double clebsch_np=0.0;
+	    
+	    if(Tab==1 && Tcd==1){
+	      clebsch_nn=1.0;
+	      clebsch_pp=1.0;
+	    }
+	    else{
+	      clebsch_nn=0.0;
+	      clebsch_pp=0.0;
+	    }
+	    
+	    clebsch_np=gsl_sf_coupling_3j(1,1,2*Tab,1,-1,0)*gsl_sf_coupling_3j(1,1,2*Tcd,1,-1,0)*sqrt(2*Tab+1)*sqrt(2*Tcd+1);
+	    
+	    double Nab=norm(ni1,li1,ji1,ni2,li2,ji2,Jab,Tab);
+	    double Ncd=norm(nj1,lj1,jj1,nj2,lj2,jj2,Jcd,Tcd);
+	    
+	    value_nn=value_nn*mult*clebsch_nn*Nab*Ncd;
+	    value_pp=value_pp*mult*clebsch_pp*Nab*Ncd;
+	    value_np=(value_np+rev1_np+rev2_np+rev3_np)*mult*clebsch_np*Nab*Ncd;
+	    
+	    
+	    if ((fabs(value_np)>0.000001) ||( fabs(value_pp)>0.000001) ||( fabs(value_nn)>0.000001) ){   //roundoff-error?
+	      if(showJtrans==1){
+		printf ("\n Jtrans= %2d\n", jtrans/2);
+		showJtrans=0;
+	      }
+#if NP_ORDER
+			    
+	      printf(" (a+a+)J=%5d  (a-a-)J=%5d   td: np=%10.6f   pp=%10.6f   nn=%10.6f\n",twob1,twob2, value_np, value_pp,value_nn);//, Jab, Tab, Jcd,Tcd);
+#else 
+	      printf(" (a+a+)J=%5d  (a-a-)J=%5d   td: pn=%10.6f   pp=%10.6f   nn=%10.6f\n",twob1,twob2, value_np, value_pp,value_nn);//, Jab, Tab, Jcd,Tcd);
+#endif
+	    }
+	  }
+	}
+      }
+    }
+
+  return 0;
+}
+typedef struct twob_state{
+  int a;
+  int b;
+  int c;
+  int d;
+  int j2;
+  int t2;
+  double hrel;
+  double trel;
+  double vpp;
+  double vnn;
+  double vpn;
+  double coul;
+} twob_state;
+
+struct twob_state *twob_array;
 int readTBME(){
 
   printf("Read TBME.int\n");
@@ -282,14 +419,29 @@ int readTBME(){
   float f[6];
   fp=fopen("TBME.int","r");
   fscanf(fp,"%d",&numTBME);
+  twob_array=malloc(sizeof(twob_state)*numTBME);
   
   for(int i=0;i<numTBME; i++){
     fscanf(fp,"%d %d %d %d %d %d %f %f %f %f %f %f",&qn[0],&qn[1],&qn[2],&qn[3],&qn[4],&qn[5],&f[0],&f[1],&f[2],&f[3],&f[4],&f[5]);
     printf("%d %d \n",qn[0],qn[1]);
-    //Identify two-body states
-    //Save matrix elements in 6 arrays (Global)
+    twob_array[i].a=qn[0];
+    twob_array[i].b=qn[1];
+    twob_array[i].c=qn[2];
+    twob_array[i].d=qn[3];
+    twob_array[i].j2=qn[4];
+    twob_array[i].t2=qn[5];
+    twob_array[i].trel=f[0];
+    twob_array[i].hrel=f[1];
+    twob_array[i].coul=f[2];
+    twob_array[i].vpn=f[3];
+    twob_array[i].vpp=f[4];
+    twob_array[i].vnn=f[5];
     
   }
   printf("%d\n",numTBME);
+
+  for (int i=0;i<numTBME;i++){
+    printf(" %d %d %d %d \n",twob_array[i].a,twob_array[i].b,twob_array[i].c,twob_array[i].d);
+  }
   return 0;
 }
